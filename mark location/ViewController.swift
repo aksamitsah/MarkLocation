d53901a179b4f39be7aa64cb55ccf2279a0e1cca
Amit Sah <aksamitsah@gmail.com>
2024-03-17 23:00:45 +0530

    Initial Commit

---
+++ mark location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mark location
+//
+//  Created by Amit Shah on 17/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
